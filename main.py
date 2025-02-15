6f5c4f37223d526aed4db5150bd3ebb7740cb86c
itsarasib <itsara.pliansart@gmail.com>
2025-02-15 18:24:02 +0700

    fix: delete users route

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from auth import auth_router
-from users import users_router
 
 app = FastAPI()
 
 # Include routers
-app.include_router(users_router.router)
 app.include_router(auth_router.router)
 
 
